a901f2adc2cea7cefd349cba55bf966026046584
Ysunil9 <sunil.pcs24@gmail.com>
2022-11-04 16:16:08 +0530

    swapping values

---
+++ SwapNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SwapNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SwapNumbers.SwapFourNumbers(3, 4, 8, 2);
+
+        }
+    }
+}
---
+++ SwapNumbers/SwapNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapNumbers
+{
+    internal class SwapNumbers
+    {
+        public static void SwapFourNumbers(int a, int b, int c, int d)
+        {
+            int temp = 0;
+            Console.WriteLine("numbers{0} {1} {2} {3} \n",a, b, c, d);
+           
+            temp = a;
+            a = b;
+            b = c;
+            c = d;
+            d = temp;
+
+
+            Console.WriteLine("swap{0} {1} {2} {3}\n", a, b, c, d);
+
+
+        }
+    }
+}
